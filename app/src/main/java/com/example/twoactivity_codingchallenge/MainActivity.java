9340560186ba1f6d621ae3d727dcdb71542290fa
surakshyabhandari <51433451+surakshyabhandari@users.noreply.github.com>
2022-03-20 12:14:20 +0545

    Lab 4 coding challenge

---
+++ app/src/main/java/com/example/twoactivity_codingchallenge/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.twoactivity_codingchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.LabeledIntent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void launchText(View view) {
+        Button textbtn = (Button) (view);
+        String text = textbtn.getText().toString();
+        Intent intent = new Intent(this, MainActivity2.class);
+        String message = "";
+        if(text == getString(R.string.text_one)){
+            message = getString(R.string.passageonetext);
+        }
+        else if (text == getString(R.string.text_two)){
+            message = getString(R.string.passagetwotext);
+        }
+        else if (text == getString(R.string.text_three)){
+            message = getString(R.string.passagethreetext);
+        }
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
